c031aef7574c30ebe97c36ea2eaf099ff9366abb
Luca Perez <lucamarper@gmail.com>
2024-05-28 17:57:39 -0300

    Agregamos contenido a java

--- java/leccion3/leccion3.java
+++ java/leccion3/leccion3.java
@@ -197,6 +197,26 @@ public class leccion3 {
     varNum1 %= 6;
     System.out.println("varNum1 = " + varNum1);
 
+    int varNum1 = 1 , varNum2 = 4;
+    int varNum3 = varNum1 + 6 - varNum2; // una operacion
+    System.out.println("varNUm3 = "  + varNum3);
+
+    varNum1 += 1; // varNum1 = varNum1 + 1;
+    System.out.println("varNum1 =" + varNum1);
+
+    varNum2 -= 2;
+    System.out.println("varNum2 = " + varNum2);
+
+    varNum1 *= 5;
+    System.out.println("varNum1 = " + varNum1);
+
+    varNum3 /= 4;
+    System.out.println("varNum3 =" + varNum3);
+
+    varNum1 %= 6;
+    System.out.println("varNum1 = " + varNum1);
+
+
     }
 
 }
