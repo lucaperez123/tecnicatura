fe4b106a5a4852e22bbbe3ffde939fc84b0c99cb
Luca Perez <lucamarper@gmail.com>
2024-05-31 17:23:53 -0300

    Agregamos ciclo while a leccion03 Python

---
+++ python/leccion03/main.py
@@ -0,0 +1,21 @@
+#ciclo while (mientras o durante)
+"""contador = 0
+while contador< 3:
+    print("EJECUTAMOS NUESTRO CICLO WHILE", contador)
+    contador +=1;
+else:
+    print ("Fin del ciclo while")"""
+
+#imprimir numero del 0 al 5 con el ciclo while (crecien)
+"""maximo = 5
+contador = 0
+while contador <= maximo:
+    print (contador)
+    contador +=1"""
+
+#ejercicio decreciente
+"""minimo = 1
+contador = 5
+while contador >= minimo:
+    print(contador)
+    contador -=1"""
