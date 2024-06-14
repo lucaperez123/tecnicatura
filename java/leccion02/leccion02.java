a35321602774d35f0bfc99f0c094fc673cd411a6
Luca Perez <lucamarper@gmail.com>
2024-06-14 12:11:29 -0300

    Agregamos sentencia de control switch con ejercitacion

--- java/leccion02/leccion02.java
+++ java/leccion02/leccion02.java
@@ -1,8 +1,10 @@
 package leccion02;
 
+import java.util.Scanner;
+
 class leccion02 {
     public static void main(String[] args) {
-        var condicion = "True";
+        /*var condicion = "True";
         if (condicion == "True"){
             System.out.println("Condición verdadera");
         } else {
@@ -27,6 +29,28 @@ class leccion02 {
         }
         else{
             numeroTexto = "Numero desconocido";
+        }
+        System.out.println("numeroTexto = " + numeroTexto);*/
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un número del 1 al 4: ");
+        var numero = Integer.parseInt(entrada.nextLine());
+        var numeroTexto = "Valor desconocido";
+        switch(numero){
+            case 1:
+                numeroTexto = "Numero uno";
+                break;
+            case 2:
+                numeroTexto = "Numero dos";
+                break;
+            case 3:
+                numeroTexto = "Numero tres";
+                break;
+            case 4:
+                numeroTexto = "Numero cuatro";
+                break;
+            default:
+                numeroTexto = "Numero desconocido";
         }
         System.out.println("numeroTexto = " + numeroTexto);
     }
