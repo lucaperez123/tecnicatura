c65605eb9289c27ad49ea183310b697443a843e6
Luca Perez <lucamarper@gmail.com>
2024-06-24 20:04:28 -0300

    Agregamos contenido de funciones a 04-01 js

--- javaScript/leccion04/04-01.js
+++ javaScript/leccion04/04-01.js
@@ -33,3 +33,49 @@ console.log (edad2);
 
 const fechaNacimiento = 2006;
 console.log (fechaNacimiento);
+
+//fechaNacimiento = 2003;
+//console.log(fechaNacimiento); // solo se ejecuta en la consola anterior
+
+//evitar repetir tu codigo
+//dry dont' repat yourself
+
+//let days =['Lunes','Martes','Miercoles' ,'Jueves', 'Vienres', 'Sabado', 'Domingo'];
+
+let days = 1;
+switch(days){
+    case 1:
+        console.log("Hoy es lunes" );
+        break;
+    case 2:
+        console.log("Hoy es martes" );
+        break;
+    case 3:
+        console.log("Hoy es miercoles" );
+        break;
+    case 4:
+        console.log("Hoy es jueves" );
+        break;
+    case 5:
+        console.log("Hoy es viernes" );
+        break;
+    case 6:
+        console.log("Hoy es sabado");
+        break;
+    case 7:
+        console.log("Hoy es domingo" );
+        break;
+    default:
+        break;
+}
+
+let days2 =['Lunes','Martes','Miercoles' ,'Jueves', 'Vienres', 'Sabado', 'Domingo'];
+
+function getday(n){
+    if(n<1 || n >7){
+        throw new error ('out of range');
+    }
+    return days2[n-1];
+}
+
+console.log(getday(5));
