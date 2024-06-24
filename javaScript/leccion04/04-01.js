06bae4865503ab65ae9fe52e43857d2322e9c128
Luca Perez <lucamarper@gmail.com>
2024-06-24 20:21:17 -0300

    Agregamos ejercicio month con switch y version mejorada con function

--- javaScript/leccion04/04-01.js
+++ javaScript/leccion04/04-01.js
@@ -69,8 +69,10 @@ switch(days){
         break;
 }
 
-let days2 =['Lunes','Martes','Miercoles' ,'Jueves', 'Vienres', 'Sabado', 'Domingo'];
 
+
+// esta es la opcion mejorada
+let days2 =['Lunes','Martes','Miercoles' ,'Jueves', 'Vienres', 'Sabado', 'Domingo'];
 function getday(n){
     if(n<1 || n >7){
         throw new error ('out of range');
@@ -79,3 +81,63 @@ function getday(n){
 }
 
 console.log(getday(5));
+
+
+// hacer un ejercicio similar al que esta hecho, pero ahora con los
+//meses del año , debes hacerlo con la estructura switch y luego
+//con la funcion en la opcion mejorada
+
+let month = 11;
+switch(month){
+    case 1:
+        console.log("Es enero");
+        break;
+    case 2:
+        console.log("Es febrero");
+        break;
+    case 3:
+        console.log("Es marzo");
+        break;
+    case 4 :
+        console.log("Es abril");
+        break;
+    case 5:
+        console.log("Es mayo");
+        break;
+    case 6:
+        console.log("Es junio");
+        break;
+    case 7:
+        console.log("Es julio");
+        break;
+    case 8:
+        console.log("Es agosto");
+        break;
+    case 9:
+        console.log("Es septiembre");
+        break;
+    case 10:
+        console.log("Es octubre");
+        break;
+    case 11:
+        console.log("Es noviembre");
+        break;
+    case 12:
+        console.log("Es diciembre");
+        break;
+    default:
+        console.log ("Error en el ingreso del mes del año");
+        break;
+}
+
+// esta es la opcion mejorada
+
+let Month2 = ['Enero','Febrero','Marzo' ,'Abril', 'Mayo', 'Junio', 'Julio' , 'Agosto','Septiembre','Octubre', 'Noviembre','Diciembre'];
+function getMonth(n){
+    if(n < 1 || n > 12){
+        throw new error("out of range");
+    }
+    return Month2[n-1];
+}
+
+console.log(getMonth(1));
